6af14fe5a13e6f96a9c96da95c3432595a158961
Mathilde Ferrand Coeurderoy <mathilde.ferrand.coeurderoy@gmail.com>
2018-08-28 16:31:30 +0100

    refactor Bookmarks tests

--- lib/bookmarks.rb
+++ lib/bookmarks.rb
@@ -1,8 +1,11 @@
 class Bookmarks
 
   def self.all
-    ['User Stories: An Agile Introduction',
-    'Writing user stories', 'Feature tests']
+    [
+      'User Stories: An Agile Introduction',
+      'Writing user stories',
+      'Feature tests'
+    ]
   end
 
 end
--- spec/bookmarks_spec.rb
+++ spec/bookmarks_spec.rb
@@ -3,7 +3,8 @@ require 'bookmarks'
 describe Bookmarks do
 
   it '.all returns a list of bookmarks' do
-    expect(Bookmarks.all).to match_array(['User Stories: An Agile Introduction',
+    bookmarks = Bookmarks.all
+    expect(bookmarks).to match_array(['User Stories: An Agile Introduction',
     'Writing user stories', 'Feature tests'])
   end
 
