f145ff865d47c0f981ab04b5763adf862ccfa9ca
davidlawes <davidlawes@hotmail.co.uk>
2018-08-29 16:56:54 +0100

    Added test database to Model and Tests

--- spec/bookmarks_spec.rb
+++ spec/bookmarks_spec.rb
@@ -2,7 +2,14 @@ require 'bookmarks'
 
 describe Bookmarks do
 
+  # describe the test subject with a double for the PG class
+
+  # describe .all
+  # it connects to a database named 'bookmark_manager_test'
+  # it passes method 'exec' to the PG object
+  # it
   it '.all returns a list of bookmarks' do
+    p ENV['RACK_ENV']
     bookmarks = Bookmarks.all
     expect(bookmarks).to match_array(["http://destroyallsoftware.com",
       "http://www.google.com", "http://www.makersacademy.com"])
