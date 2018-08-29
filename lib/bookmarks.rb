3fd146c3c8203669b2ac3b826f57fcf6439e9d30
davidlawes <davidlawes@hotmail.co.uk>
2018-08-29 14:59:40 +0100

    Renaming in bookmarks.all map method

--- lib/bookmarks.rb
+++ lib/bookmarks.rb
@@ -4,7 +4,7 @@ class Bookmarks
 
   def self.all
     conn = PG.connect( dbname: 'bookmark_manager')
-    conn.exec( "SELECT * FROM bookmarks" ).map { |elem| elem["url"] }
+    conn.exec( "SELECT * FROM bookmarks" ).map { |bookmark| bookmark["url"] }
   end
 
 end
