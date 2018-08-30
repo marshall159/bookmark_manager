23a1624957b3f021a2d7f973e7dbda290bc817b7
josh nickson <jnickson1506@gmail.com>
2018-08-30 16:47:05 +0100

    add .create method

--- lib/bookmarks.rb
+++ lib/bookmarks.rb
@@ -3,14 +3,23 @@ require 'pg'
 class Bookmarks
 
   def self.all
+    connect_database
+    @conn.exec( "SELECT * FROM bookmarks" ).map { |bookmark| bookmark["url"] }
+  end
+
+  def self.create(url)
+    connect_database
+    @conn.exec( "INSERT INTO bookmarks(url) 
+                VALUES ('#{url}');" )
+  end
+
+  def self.connect_database
     if ENV['ENVIRONMENT'] == 'test'
       database = 'bookmark_manager_test'
     else
       database = 'bookmark_manager'
     end
-
-    conn = PG.connect( dbname: database)
-    conn.exec( "SELECT * FROM bookmarks" ).map { |bookmark| bookmark["url"] }
+    @conn = PG.connect( dbname: database)
   end
 
 end
--- spec/bookmarks_spec.rb
+++ spec/bookmarks_spec.rb
@@ -1,10 +1,21 @@
 require 'bookmarks'
 
 describe Bookmarks do
-  it '.all returns a list of bookmarks' do
-    bookmarks = Bookmarks.all
-    expect(bookmarks).to match_array(["http://destroyallsoftware.com",
-      "http://www.google.com", "http://www.makersacademy.com"])
+
+  describe '.all' do
+    it 'returns a list of bookmarks' do
+      bookmarks = Bookmarks.all
+      expect(bookmarks).to match_array(["http://destroyallsoftware.com",
+        "http://www.google.com", "http://www.makersacademy.com"])
+    end
+  end
+
+  describe '.create' do
+    it 'adds a new bookmark to the list of bookmarks' do
+      Bookmarks.create("http://football365.com")
+      bookmarks = Bookmarks.all
+      expect(bookmarks).to include("http://football365.com")
+    end
   end
 
 end
